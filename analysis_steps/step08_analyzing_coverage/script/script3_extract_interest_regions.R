54115e43ff7b30b99e9c036ac4c6ff05fc18d881
etiennejaussi <118220833+etiennejaussi@users.noreply.github.com>
2023-05-10 15:29:21 -0400

    change object name

--- analysis_steps/step08_analyzing_coverage/script/script3_extract_interest_regions.R
+++ analysis_steps/step08_analyzing_coverage/script/script3_extract_interest_regions.R
@@ -32,7 +32,7 @@ print(cov_deseq %>% arrange(desc(log2FoldChange)), width = Inf)
 print(cov_deseq %>% arrange(padj), width = Inf)
 
 # create a table that contain all regions of interest
-interest_regions_all <- cov_deseq %>% select(chro, start, end)
+all_interest_regions <- cov_deseq %>% select(chro, start, end)
 
 # filter to obtain a table that contain data with an adjusted p-value < 0.05
 cov_deseq_sig <- cov_deseq %>% filter(padj < 0.05)
@@ -42,9 +42,9 @@ print(cov_deseq_sig, width = Inf)
 print(cov_deseq_sig %>% arrange(padj), width = Inf)
 
 # create a table that contain significant regions of interest
-interest_regions_sig <- cov_deseq_sig %>% select(chro, start, end)
+sig_interest_regions <- cov_deseq_sig %>% select(chro, start, end)
 
 # save the the tables that contain all and significant regions of interest in a text format
-write.table(interest_regions_all, file="output/output3_extract_interest_regions/all_interest_regions.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
-write.table(interest_regions_sig, file="output/output3_extract_interest_regions/sig_interest_regions.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+write.table(all_interest_regions, file="output/output3_extract_interest_regions/all_interest_regions.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
+write.table(sig_interest_regions, file="output/output3_extract_interest_regions/sig_interest_regions.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
 
